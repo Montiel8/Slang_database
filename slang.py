b60e6b4f6717124dbe1f966370af48f3d034dec4
Montiel8 <montielarturo47@gmail.com>
2023-11-16 08:56:14 -0700

    Program

---
+++ slang.py
@@ -0,0 +1,13 @@
+"""
+Name: Arturo Montiel 
+Date: 11/15/23
+Purpose: Show understadning of SQL as a database 
+        and work it with python."""
+
+#TODO: Create title 
+# Let the user know what is happening 
+#TODO: Create the table in sql 
+# Let the user know what is happening 
+#TODO: Put informatin in SQL 
+# Let the user know what is happening 
+#TODO: Close the connection 
